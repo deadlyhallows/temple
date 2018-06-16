dd481856494c3ff212debc4a25abc7d751b0aa3c
deadlyhallows <awanti2019@gmail.com>
2018-06-16 19:32:35 +0530

    removed venv folder

--- venv/lib/python3.6/site-packages/notify/tests/urls.py
+++
@@ -1,8 +0,0 @@
-try:
-    from django.conf.urls import url, include
-except ImportError:
-    from django.urls import url, include
-
-urlpatterns = [
-    url(r'^', include('notify.urls', namespace='notifications')),
-]
