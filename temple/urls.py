de7feac555dc0f7910ef1622f316efaf56ec153e
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2018-07-04 23:12:36 +0530

    setting debug=false for deployment

--- temple/urls.py
+++ temple/urls.py
@@ -44,10 +44,10 @@ handler404 = core_views.error404  # noqa
 handler500 = core_views.error500  # noqa
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# if settings.DEBUG is False:
-#     urlpatterns += [url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
-#                     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}), ]
+if settings.DEBUG is False:
+    urlpatterns += [url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
+                    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}), ]
