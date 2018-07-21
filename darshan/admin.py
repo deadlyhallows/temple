e8f5dca4cab7e11ac3dba4b255c2bdd1cd605e45
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2018-07-21 10:37:43 +0530

    Making Pop-Up Responsive

--- darshan/admin.py
+++ darshan/admin.py
@@ -1,4 +1,4 @@
-from .models import Picture, Profile, Temples, Darshans, Mobile,TempleManager
+from .models import Picture, Profile, Temples, Darshans, Mobile,TempleManager, ContactInspire, Pandit
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
@@ -55,6 +55,8 @@ admin.site.register(Mobile)
 admin.site.register(Darshans)
 admin.site.register(Picture, PictureAdmin)
 admin.site.register(Profile)
+admin.site.register(ContactInspire)
+admin.site.register(Pandit)
 
 #class ProfileInline(admin.StackedInline):
  #   model = Profile
